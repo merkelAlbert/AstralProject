4c73c0eada057d130c450a0a0203d57efed416f5
merkelAlbert <merkel.albert1997@gmail.com>
2017-10-11 10:28:24 +0300

    README added

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"AstralProject/ConfigTypes"
 	"AstralProject/FileManager"
 	"AstralProject/Logger"
+	"AstralProject/github.com/lib/pq"
+	"database/sql"
 	"encoding/xml"
 	"fmt"
 	"os"
@@ -14,13 +16,10 @@ func main() {
 	var configPath string
 	var loggerPath string
 	var config []byte
-
 	loggerPath, configPath = cmdParser.GetPathsFromCommandLine()
 	logger.SetLogger(loggerPath)
-
 	config = fileManager.Read(configPath, os.O_RDONLY, 0444)
 	var data configTypes.Data
-
 	xml.Unmarshal(config, &data)
 	for _, v := range data.PersonList {
 		fmt.Println(v)
